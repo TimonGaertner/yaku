907d738d987ae4247cbf0a9fbc8475fa3a8e11a4
TimonGaertner <timon.ben.gaertner@gmail.com>
2022-01-01 20:07:48 +0100

    pragma once

--- kernel/misc/keyboard_stringmap.h
+++ kernel/misc/keyboard_stringmap.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <types.h>
 
-extern const char* keyboard_stringmap[105][3]; // column: 1.char without shift or alt, 2.char with shift, 3.char with altgr
+extern const char* keyboard_stringmap[105][3]; // column: 1.char without shift or alt, 2.char with shift, 3.char with altgr https://de.wikipedia.org/wiki/Tastaturbelegung#Tastaturbelegung_T1
