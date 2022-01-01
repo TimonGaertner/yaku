f495490703a2153efff23af27559a46d94c61ddb
TimonGaertner <timon.ben.gaertner@gmail.com>
2022-01-01 21:19:14 +0100

    function naming

--- kernel/lib/string.c
+++ kernel/lib/string.c
@@ -1,7 +1,7 @@
 #include "string.h"
 #include <types.h>
 
-bool string_strcmp(char* str1, char* str2) {
+bool strcmp(char* str1, char* str2) {
     while (*str1 == *str2) {
         if (*str1 == '\0') {
             return true;
@@ -12,7 +12,7 @@ bool string_strcmp(char* str1, char* str2) {
     return false;
 }
 
-void string_memcpy(void* dest, const void* src, size_t n) {
+void memcpy(void* dest, const void* src, size_t n) {
     for (size_t i = 0; i < n; i++) {
         ((char*)dest)[i] = ((char*)src)[i];
     }
@@ -21,7 +21,7 @@ void string_memcpy(void* dest, const void* src, size_t n) {
 /**
  * @return size_t length of string, not counting further than maxlen
  */
-size_t string_strnlen(const char* s, size_t maxlen) {
+size_t strnlen(const char* s, size_t maxlen) {
     const char* p = s;
     while (maxlen-- && *p) {
         p++;
@@ -29,10 +29,10 @@ size_t string_strnlen(const char* s, size_t maxlen) {
     return p - s;
 }
 
-/** 
+/**
  * @return size_t length of string
  */
-size_t string_strlen(const char* s) {
+size_t strlen(const char* s) {
     const char* p = s;
     while (*p) {
         p++;
@@ -40,12 +40,12 @@ size_t string_strlen(const char* s) {
     return p - s;
 }
 
-char* string_strncat(char* s1, const char* s2, size_t n) {
+char* strncat(char* s1, const char* s2, size_t n) {
     char* s = s1;
     /* Find the end of S1.  */
-    s1 += string_strlen(s1);
-    size_t ss = string_strnlen(s2, n);
+    s1 += strlen(s1);
+    size_t ss = strnlen(s2, n);
     s1[ss] = '\0';
-    string_memcpy(s1, s2, ss);
+    memcpy(s1, s2, ss);
     return s;
 }
